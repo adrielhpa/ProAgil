a8e1e4956da93ebb5561cd47aa9aee9fba348f71
Adriel <adriel_hp.andrade@outlook.com>
2021-04-23 22:49:11 -0300

    Adding Angular Project SPA

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -30,7 +30,10 @@ namespace ProAgil.API
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +50,7 @@ namespace ProAgil.API
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseMvc();
         }
     }
